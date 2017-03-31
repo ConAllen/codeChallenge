634658962d1fa13c0fc778dfa80fca87c30c94fe
Con <allenconallen46@gmail.com>
2017-03-31 14:22:59 +0100

    names to users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #this below is code taken from the devise website.
+  #it adds strong perameters to our sign in. we are adding names to sign in accounts
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
